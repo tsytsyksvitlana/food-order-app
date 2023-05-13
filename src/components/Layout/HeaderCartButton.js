97797d09ea409bf03d12b13e074f9d3f032d1a89
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-05-13 22:59:44 +0300

    updated project

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,25 @@
-import { useContext } from "react";
-import CartIcon from "../Cart/CartIcon";
-import CartContex from "../../store/cart-context";
+import { useContext } from 'react';
+
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../../../../../food-order-app/store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton=props=>{
-    const cartCtx=useContext(CartContex);
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems=cartCtx.items.reduce((curNumber, item)=>{
-        return curNumber+item.amount;
-    }, 0);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
